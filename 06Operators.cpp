a26c81afbb047350085bbc2e6c90ad2bdb67e608
Ajay Kumar <ajaykumarjakhar08@gmail.com>
2025-06-24 09:55:00 +0000

    3rd

---
+++ 06Operators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter 1st no : ";
+    cin>>a;
+    cout<<"Enter 2nd no : ";
+    cin>>b;
+    c = a+b;
+    int d = a-b;
+    int e = a*b;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+    return 0;
+}
---
+++ 07Bitwise.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 5;
+    cout<<(a>>3)<<endl; // it means left shift by 3 (means /2/2/2)
+    cout<<(a<<3); // it means right shift by 3 (means *2*2*2)
+    return 0;
+}
