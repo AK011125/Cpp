b007f9a11fc4f336a588d3d1b08dc79cac9d3b86
Ajay Kumar <ajaykumarjakhar08@gmail.com>
2025-06-24 06:07:38 +0000

    1st c++

---
+++ 02Variables.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hiiiiiiii"<<endl; //<<endl is used for printing in next line 
+    cout<<"Hlooooooo";    //cout<< is used for printing
+    return 0;
+}
